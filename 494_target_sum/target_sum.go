86224b37f1b8d9f580227a4bcd34b1ae0c718ba9
Akos Hajas <hakosh2@gmail.com>
2022-01-13 09:44:50 +0100

    494: Target Sum

--- 494_target_sum/target_sum.go
+++ 494_target_sum/target_sum.go
@@ -1,1 +1,19 @@
-package _94_target_sum
+package target_sum
+
+func findTargetSumWays(nums []int, target int) int {
+	return ways(nums, target)
+}
+
+func ways(nums []int, target int) int {
+	if len(nums) == 0 {
+		if target == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	num := nums[0]
+
+	return ways(nums[1:], target+num) + ways(nums[1:], target-num)
+}
--- 494_target_sum/target_sum_test.go
+++ 494_target_sum/target_sum_test.go
@@ -1,1 +1,27 @@
-package _94_target_sum
+package target_sum
+
+import "testing"
+
+type Test struct {
+	in     []int
+	target int
+	out    int
+}
+
+var tests = []Test{
+	{[]int{1, 1, 1, 1, 1}, 3, 5},
+}
+
+func TestFindTargetSumWays(t *testing.T) {
+	for _, test := range tests {
+		if res := findTargetSumWays(test.in, test.target); res != test.out {
+			t.Errorf("For %v and %v exptected %v, got %v", test.in, test.target, test.out, res)
+		}
+	}
+}
+
+func BenchmarkFindTargetSumWays(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		findTargetSumWays(tests[0].in, 3)
+	}
+}
