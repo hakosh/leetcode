018387bc8eb69bddfc4b9d69eef3151e964a129f
Akos Hajas <hakosh2@gmail.com>
2022-01-14 13:49:56 +0100

    145: Binary Tree Postorder Traversal - iterative

--- 145_binary_tree_postorder_traversal/binary_tree_postorder_traversal.go
+++ 145_binary_tree_postorder_traversal/binary_tree_postorder_traversal.go
@@ -6,15 +6,46 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func postorderTraversal(root *TreeNode) []int {
+func postorderTraversalRecursive(root *TreeNode) []int {
 	if root == nil {
 		return []int{}
 	}
 
 	var list []int
-	list = append(list, postorderTraversal(root.Left)...)
-	list = append(list, postorderTraversal(root.Right)...)
+	list = append(list, postorderTraversalRecursive(root.Left)...)
+	list = append(list, postorderTraversalRecursive(root.Right)...)
 	list = append(list, root.Val)
+
+	return list
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	list := make([]int, 0)
+
+	curr := root
+	stack := make([]*TreeNode, 0)
+
+	for curr != nil || len(stack) > 0 {
+		for curr != nil {
+			stack = append(stack, curr)
+
+			if curr.Left != nil {
+				curr = curr.Left
+			} else {
+				curr = curr.Right
+			}
+		}
+
+		curr = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		list = append(list, curr.Val)
+
+		if len(stack) != 0 && curr == stack[len(stack)-1].Left && stack[len(stack)-1].Right != nil {
+			curr = stack[len(stack)-1].Right
+		} else {
+			curr = nil
+		}
+	}
 
 	return list
 }
