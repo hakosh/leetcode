74e535489b0675e4d577cca2c5c7b395ab88ace5
Akos Hajas <hakosh2@gmail.com>
2022-01-14 12:11:12 +0100

    144: Binary Tree Preorder Traversal - iterative

--- 144_binary_tree_preorder_traversal/binary_tree_preorder_traversal.go
+++ 144_binary_tree_preorder_traversal/binary_tree_preorder_traversal.go
@@ -6,14 +6,36 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func preorderTraversal(root *TreeNode) []int {
+func preorderTraversalRecursive(root *TreeNode) []int {
 	if root == nil {
 		return []int{}
 	}
 
 	list := []int{root.Val}
-	list = append(list, preorderTraversal(root.Left)...)
-	list = append(list, preorderTraversal(root.Right)...)
+	list = append(list, preorderTraversalRecursive(root.Left)...)
+	list = append(list, preorderTraversalRecursive(root.Right)...)
+
+	return list
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	list := make([]int, 0)
+
+	curr := root
+	var stack []*TreeNode
+
+	for curr != nil || len(stack) > 0 {
+		for curr != nil {
+			list = append(list, curr.Val)
+			stack = append(stack, curr)
+			curr = curr.Left
+		}
+
+		curr = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		curr = curr.Right
+	}
 
 	return list
 }
