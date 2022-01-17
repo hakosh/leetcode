a2cb897d30df4f1f4ce5941b4837385e700c4456
Akos Hajas <hakosh2@gmail.com>
2022-01-17 18:47:29 +0100

    21: Merge Two Sorted Lists - recursive

--- 21_merge_two_sorted_lists/merge_two_sorted_lists.go
+++ 21_merge_two_sorted_lists/merge_two_sorted_lists.go
@@ -5,7 +5,7 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+func mergeTwoListsIterative(list1 *ListNode, list2 *ListNode) *ListNode {
 	merged := &ListNode{Val: -1}
 	mergedHead := merged
 
@@ -25,4 +25,26 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	}
 
 	return mergedHead.Next
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	var head *ListNode
+
+	if list1.Val < list2.Val {
+		head = list1
+		head.Next = mergeTwoLists(list1.Next, list2)
+	} else {
+		head = list2
+		head.Next = mergeTwoLists(list1, list2.Next)
+	}
+
+	return head
 }
