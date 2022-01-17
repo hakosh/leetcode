6cf697ad260d3da320d6528088a67e41a3780f7a
Akos Hajas <hakosh2@gmail.com>
2022-01-17 12:38:23 +0100

    24: Swap Nodes in Pairs

--- 24_swap_nodes_in_pairs/swap_nodes_in_pairs.go
+++ 24_swap_nodes_in_pairs/swap_nodes_in_pairs.go
@@ -1,1 +1,20 @@
-package _4_swap_nodes_in_pairs
+package swap_nodes_in_pairs
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	next := head.Next
+	from := head.Next.Next
+
+	head.Next.Next = head
+	head.Next = swapPairs(from)
+
+	return next
+}
--- 24_swap_nodes_in_pairs/swap_nodes_in_pairs_test.go
+++ 24_swap_nodes_in_pairs/swap_nodes_in_pairs_test.go
@@ -1,1 +1,43 @@
-package _4_swap_nodes_in_pairs
+package swap_nodes_in_pairs
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Test struct {
+	in  *ListNode
+	out []int
+}
+
+var l1 = &ListNode{
+	Val: 1,
+	Next: &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val:  3,
+			Next: &ListNode{Val: 4},
+		},
+	},
+}
+
+var tests = []Test{
+	{l1, []int{2, 1, 4, 3}},
+}
+
+func TestDecodeString(t *testing.T) {
+	for _, test := range tests {
+		l := swapPairs(test.in)
+		if !reflect.DeepEqual(traverse(l), test.out) {
+			t.Errorf("For %v expected %v, got %v", test.in, test.out, traverse(l))
+		}
+	}
+}
+
+func traverse(head *ListNode) []int {
+	if head == nil {
+		return []int{}
+	}
+
+	return append([]int{head.Val}, traverse(head.Next)...)
+}
