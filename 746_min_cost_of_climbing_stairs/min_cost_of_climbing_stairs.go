69254c81461c446d8fe99b9be42d5bb4d9507e87
Akos Hajas <hakosh2@gmail.com>
2022-01-20 01:04:35 +0100

    746: Min Cost Climbing Stairs - iterative

--- 746_min_cost_of_climbing_stairs/min_cost_of_climbing_stairs.go
+++ 746_min_cost_of_climbing_stairs/min_cost_of_climbing_stairs.go
@@ -1,9 +1,25 @@
 package min_cost_of_climbing_stairs
 
+import "fmt"
+
 var m map[int]int
 
 func minCostClimbingStairs(cost []int) int {
+	downTwo, downOne := 0, 0
+
+	for i := 2; i < len(cost)+1; i++ {
+		downOne, downTwo = min(downTwo+cost[i-2], downOne+cost[i-1]), downOne
+	}
+
+	return downOne
+}
+
+func minCostClimbingStairsR(cost []int) int {
 	m = make(map[int]int, len(cost))
+
+	defer func() {
+		fmt.Println(m)
+	}()
 
 	m[0] = cost[0]
 	m[1] = cost[1]
