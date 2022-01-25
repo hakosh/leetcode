af0dddb0bd143943ede447459bcd4983693b7999
Akos Hajas <hakosh2@gmail.com>
2022-01-25 22:32:04 +0100

    746: Min Cost of Climbing Stairs - state reduction

--- 746_min_cost_of_climbing_stairs/min_cost_of_climbing_stairs.go
+++ 746_min_cost_of_climbing_stairs/min_cost_of_climbing_stairs.go
@@ -1,31 +1,20 @@
 package min_cost_of_climbing_stairs
 
-import "fmt"
-
 var m map[int]int
 
 func minCostClimbingStairs(cost []int) int {
-	cost = append(cost, 0)
-
-	ln := len(cost)
-	dp := make([]int, ln+1)
-
-	dp[0] = cost[0]
-	dp[1] = cost[1]
+	downOne := cost[0]
+	downTwo := cost[1]
 
 	for i := 2; i < len(cost); i++ {
-		dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+		downOne, downTwo = downTwo, min(downTwo, downOne)+cost[i]
 	}
 
-	return dp[ln-1]
+	return min(downTwo, downOne)
 }
 
 func minCostClimbingStairsR(cost []int) int {
 	m = make(map[int]int, len(cost))
-
-	defer func() {
-		fmt.Println(m)
-	}()
 
 	m[0] = cost[0]
 	m[1] = cost[1]
