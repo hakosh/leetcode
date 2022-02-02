ebe904d35e01b47e04e440af4ceff0ed6a56c4fe
Akos Hajas <hakosh2@gmail.com>
2022-02-02 20:47:13 +0100

    384: Shuffle an Array

--- 384_shuffle_an_array/shuffle_an_array.go
+++ 384_shuffle_an_array/shuffle_an_array.go
@@ -1,1 +1,30 @@
-package _84_shuffle_an_array
+package shuffle_an_array
+
+import "math/rand"
+
+type Solution struct {
+	nums     []int
+	shuffled []int
+}
+
+func Constructor(nums []int) Solution {
+	solution := Solution{nums: nums, shuffled: make([]int, len(nums))}
+	copy(solution.shuffled, nums)
+
+	return solution
+}
+
+func (this *Solution) Reset() []int {
+	return this.nums
+}
+
+func (this *Solution) Shuffle() []int {
+	copy(this.shuffled, this.nums)
+
+	for i := 0; i < len(this.shuffled); i++ {
+		idx := rand.Intn(len(this.shuffled))
+		this.shuffled[i], this.shuffled[idx] = this.shuffled[idx], this.shuffled[i]
+	}
+
+	return this.shuffled
+}
--- 384_shuffle_an_array/shuffle_an_array_test.go
+++ 384_shuffle_an_array/shuffle_an_array_test.go
@@ -1,1 +1,42 @@
 package shuffle_an_array
+
+import (
+	"math/rand"
+	"reflect"
+	"testing"
+)
+
+type Test struct {
+	in  []int
+	out []int
+}
+
+var tests = []Test{
+	{[]int{1, 2, 3}, []int{2, 3, 1}},
+	{[]int{4, 5, 6, 7, 8, 9}, []int{5, 8, 4, 6, 7, 9}},
+}
+
+func TestSolution(t *testing.T) {
+	for _, test := range tests {
+		rand.Seed(1)
+		s := Constructor(test.in)
+
+		if res := s.Shuffle(); !reflect.DeepEqual(res, test.out) {
+			t.Errorf("For %v expected %v, got %v", test.in, test.out, res)
+		}
+	}
+}
+
+func TestSolution_Reset(t *testing.T) {
+	nums := tests[1].in
+
+	s := Constructor(nums)
+
+	if reflect.DeepEqual(nums, s.Shuffle()) {
+		t.Errorf("Array was not shuffled")
+	}
+
+	if !reflect.DeepEqual(nums, s.Reset()) {
+		t.Errorf("Array was not reset")
+	}
+}
