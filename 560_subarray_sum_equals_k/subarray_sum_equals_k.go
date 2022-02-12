d392d3bcc32d954573ca39bfdead7093d1e4898f
Akos Hajas <hakosh2@gmail.com>
2022-02-12 19:41:15 +0100

    560: Subarray Sum Equals K

--- 560_subarray_sum_equals_k/subarray_sum_equals_k.go
+++ 560_subarray_sum_equals_k/subarray_sum_equals_k.go
@@ -1,1 +1,21 @@
-package _60_subarray_sum_equals_k
+package subarray_sum_equals_k
+
+func subarraySum(nums []int, k int) int {
+	freq := make(map[int]int)
+	freq[0] = 1
+
+	count, sum := 0, 0
+
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+		v, ok := freq[sum-k]
+
+		if ok {
+			count += v
+		}
+
+		freq[sum]++
+	}
+
+	return count
+}
--- 560_subarray_sum_equals_k/subarray_sum_equals_k_test.go
+++ 560_subarray_sum_equals_k/subarray_sum_equals_k_test.go
@@ -1,1 +1,23 @@
-package _60_subarray_sum_equals_k
+package subarray_sum_equals_k
+
+import "testing"
+
+type Test struct {
+	nums []int
+	k    int
+	out  int
+}
+
+var tests = []Test{
+	{[]int{1, 1, 1}, 2, 2},
+	{[]int{1, 2, 3}, 3, 2},
+	{[]int{2, 2, -2, 2}, 4, 2},
+}
+
+func TestSubarraySum(t *testing.T) {
+	for _, test := range tests {
+		if res := subarraySum(test.nums, test.k); res != test.out {
+			t.Errorf("For %v and %v expected %v, got %v", test.nums, test.k, test.out, res)
+		}
+	}
+}
