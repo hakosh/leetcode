9391da7a67b13a828a12bfce445c29b30b2cdf66
Akos Hajas <hakosh2@gmail.com>
2022-02-22 13:45:45 +0100

    84: Largest Rectangle in Histogram - optimized

--- 84_largest_rectangle_in_histogram/largest_rectangle_in_histogram.go
+++ 84_largest_rectangle_in_histogram/largest_rectangle_in_histogram.go
@@ -2,7 +2,39 @@ package largest_rectangle_in_histogram
 
 func largestRectangleArea(heights []int) int {
 	area := 0
-	stack := make([][2]int, 1)
+	stack := make([]int, 1)
+	stack[0] = -1
+
+	for i := 0; i < len(heights); i++ {
+		for len(stack) > 1 && heights[stack[len(stack)-1]] >= heights[i] {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			this := heights[top] * (i - stack[len(stack)-1] - 1)
+			if this > area {
+				area = this
+			}
+		}
+
+		stack = append(stack, i)
+	}
+
+	for len(stack) > 1 {
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		this := heights[top] * (len(heights) - stack[len(stack)-1] - 1)
+		if this > area {
+			area = this
+		}
+	}
+
+	return area
+}
+
+func largestRectangleArea2(heights []int) int {
+	area := 0
+	stack := make([][2]int, 1, 16)
 	stack[0] = [2]int{0, -1}
 
 	for i := 0; i <= len(heights); i++ {
