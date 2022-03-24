1f33902840c021489f7369ff88f29f479699a32b
Akos Hajas <hakosh2@gmail.com>
2022-03-24 19:01:15 +0100

    881: Boats to Save People - update

--- 881_boats_to_save_people/boats_to_save_people.go
+++ 881_boats_to_save_people/boats_to_save_people.go
@@ -4,29 +4,18 @@ import "sort"
 
 func numRescueBoats(people []int, limit int) int {
 	sort.Ints(people)
+
+	boats := 0
 	left, right := 0, len(people)-1
-	boats, current, count := 0, 0, 0
 
-	for left-right != 1 {
-		if count == 2 {
-			boats, current, count = boats+1, 0, 0
-		}
-
-		if current+people[right] <= limit {
-			current += people[right]
-			count++
-			right--
-		} else if current+people[left] <= limit {
-			current += people[left]
-			count++
-			left++
-		} else {
-			boats, current, count = boats+1, 0, 0
-		}
-	}
-
-	if current != 0 {
+	for left <= right {
 		boats++
+
+		if people[left]+people[right] <= limit {
+			left++
+		}
+
+		right--
 	}
 
 	return boats
